9e807f8bcdf4a726616e3f5155d4119d49dba1b9
ioanasilas <ioana.silas01@e-uvt.ro>
2026-02-09 08:42:49 +0000

    Update ephemeral test, add durable consumer test

--- java-tlamonitor-audit/src/test/java/tla/monitor/audit/AppTest.java
+++ java-tlamonitor-audit/src/test/java/tla/monitor/audit/AppTest.java
@@ -13,18 +13,18 @@ import tlc2.value.IValue;
 import tlc2.value.impl.TupleValue;
 import tlc2.value.impl.Value;
 
-@Ignore("Disabled during packaging")
+// @Ignore("Disabled during packaging")
 
 public class AppTest {
-
+    private static final String subject = "audit.multitenancy";
+    private static final String durable = "junit-durable-test";
     @Test
-    public void consumeNatsMessages() {
-        try {
-            String subject = "audit.node.per.tenant";
+    public void ephConsumerTest() throws Exception {
             List<IValue> messages = new ArrayList<>();
-            ConsumerContext ephemeralContext = tlc2.NatsManager.getEphemeralConsumer(subject);
+            ConsumerContext ephemeralContext = tlc2.NatsClient.getEphemeralConsumer(subject);
             FetchConsumer fetchConsumer = ephemeralContext.fetchMessages(50);
             Message msg;
+
             while ((msg = fetchConsumer.nextMessage()) != null) {
                 byte[] msgData = msg.getData();
                 System.out.println("Msgdata size=" + msgData.length);
@@ -33,13 +33,39 @@ public class AppTest {
                 System.out.println("Json type: " + jsonMessage.getNodeType());
                 IValue tlaValue = Utils.getValueFromJson(jsonMessage);
                 System.out.println("TLA+ value: " + tlaValue.toString());
+                String tlaType = jsonMessage.path("tlaType").asText(null);
+                System.out.println("tlaType= " + tlaType);
                 msg.ack();
                 messages.add(tlaValue);
             }
-            TupleValue result = new TupleValue(messages.toArray(new Value[0]));
-            System.out.println("Result: " + result.toString());
-        } catch (Exception e) {
-            e.printStackTrace();
+            // TupleValue result = new TupleValue(messages.toArray(new Value[0]));
+            // System.out.println("Result: " + result.toString());
+    }
+
+    @Test
+    public void durConsumerTest() throws Exception {
+        ConsumerContext durableCtx = tlc2.NatsClient.getDurableConsumer(durable, subject);
+
+        FetchConsumer fc = durableCtx.fetchMessages(10);
+        Message msg;
+        int acked = 0;
+
+        while ((msg = fc.nextMessage()) != null) {
+            JsonNode json = Utils.parseAndGetJson(msg.getData());
+            String tlaType = json.path("tlaType").asText(null);
+            System.out.println("tlaType= " + tlaType);
+
+            msg.ack();
+            acked++;
+        }
+
+        System.out.println("Acked " + acked);
+
+        FetchConsumer fc2 = durableCtx.fetchMessages(10);
+        Message msg2 = fc2.nextMessage();
+
+        if (msg2 != null) {
+            throw new AssertionError("Expected no new messages but got one, why?");
         }
     }
 }
