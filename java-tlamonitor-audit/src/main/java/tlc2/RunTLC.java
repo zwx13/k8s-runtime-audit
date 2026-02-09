e7a9e6fa4b624d2244119cdcb46f5cbf9189d6ee
ioanasilas <ioana.silas01@e-uvt.ro>
2026-02-09 08:51:10 +0000

    Clean-up RunTLC.java class

--- java-tlamonitor-audit/src/main/java/tlc2/RunTLC.java
+++ java-tlamonitor-audit/src/main/java/tlc2/RunTLC.java
@@ -7,19 +7,25 @@ import io.nats.client.JetStreamApiException;
 
 public class RunTLC {
     static int runTLC(String specFile, String cfgFile, String tlaToolsPath, String overridesJar) throws IOException, InterruptedException, JetStreamApiException{
+    String tlaToolsPathAbs = new File(tlaToolsPath).getAbsolutePath();
+    String overridesAbs = new File(overridesJar).getAbsolutePath();
+    String cfgAbs = new File(cfgFile).getAbsolutePath();
+    String specAbs = new File(specFile).getAbsolutePath();
+
     ProcessBuilder pb = new ProcessBuilder(
     "java",
-    "-cp", tlaToolsPath + ":" + overridesJar,
-    "-DTLA-Library=" + overridesJar,
+    "-cp", tlaToolsPathAbs + ":" + overridesAbs,
+    "-DTLA-Library=" + overridesAbs,
     "tlc2.TLC",
     "-continue",
-    "-config", new File(cfgFile).getAbsolutePath(),
-    new File(specFile).getAbsolutePath()
+    "-config", cfgAbs,
+    specAbs
 );
 
     pb.inheritIO();
     Process process = pb.start();
     int exitCode = process.waitFor();
+    
     System.out.println("TLC exited with code: " + exitCode);
     return exitCode;
     }
