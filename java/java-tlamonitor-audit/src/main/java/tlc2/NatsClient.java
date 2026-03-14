6a2c3f13c46b0c8a0ef1c20411e6ed29fbc22d80
ioanasilas <ioana.silas01@e-uvt.ro>
2026-03-14 16:34:30 +0000

    fix: NATS_URL should refer to internal docker network name We plan on always running this with docker.

--- java/java-tlamonitor-audit/src/main/java/tlc2/NatsClient.java
+++ java/java-tlamonitor-audit/src/main/java/tlc2/NatsClient.java
@@ -21,7 +21,7 @@ public class NatsClient {
     // fields of the class are accessed
     static {
         try {
-            NATS_URL = Utils.env("NATS_URL", "nats://127.0.0.1:4222");
+            NATS_URL = Utils.env("NATS_URL", "nats://nats:4222");
             STREAM = Utils.env("JS_STREAM", "AUDIT_MT");
 
             Options opts = new Options.Builder()
